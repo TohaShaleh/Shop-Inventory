22a34bfc792e777da66b44e97aa2a6056af32065
Abu Shaleh <abushalehtoha@gmail.com>
2025-01-10 18:45:47 +0600

    Removed submodule and added Back_End as part of the repository

---
+++ Back_End/src/main/java/com/shop/inventory/ShopInventoryApplication.java
@@ -0,0 +1,11 @@
+package com.shop.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopInventoryApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ShopInventoryApplication.class, args);
+	}
+}
